dfb3d66305fdfa0920bc3534dbeeeee5e40444ba
Aszmel <mellipone@gmail.com>
2020-10-17 18:27:03 +0100

    added normalize.css and GlobalStyles from styled-components

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,4 +1,7 @@
-var plugins = []
+var plugins = [{
+      plugin: require('C:/Users/holde/master-gatsby/gatsby/gatsby-ssr'),
+      options: {"plugins":[]},
+    }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
 //   {
